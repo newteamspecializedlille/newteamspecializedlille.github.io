a0dbca1e2bdd3d4d0c93fa1e2868f78f13fe507b
Felix <felix.moulin@decathlon.com>
2025-02-03 16:51:41 +0100

    end

--- python_scripts/rando.py
+++ python_scripts/rando.py
@@ -1,7 +1,8 @@
+import locale
+from datetime import datetime
 import requests
 import csv
 import json
-from datetime import datetime
 
 # ID de la feuille Google Sheet (tiré de l'URL)
 SPREADSHEET_ID = '1ZxmM4AjqzwlpqFPdoUY68f5fLKYDoTGJjzFQwMWKDRE'
@@ -11,7 +12,8 @@ CSV_URL = f'https://docs.google.com/spreadsheets/d/{SPREADSHEET_ID}/export?forma
 
 def parse_date(date_str):
     """Parse une date au format attendu ou retourne une erreur si invalide."""
-    formats = ['%d/%m/%Y']  # Gère les formats abrégés et longs
+    locale.setlocale(locale.LC_TIME, 'fr_FR.UTF-8')  # Définit la locale en français
+    formats = ['%d/%m/%Y', '%A %d %B %Y']  # Ajoute le format avec le jour de la semaine
     for fmt in formats:
         try:
             return datetime.strptime(date_str, fmt)
@@ -31,8 +33,8 @@ def rando():
     response = requests.get(CSV_URL)
     response.raise_for_status()  # Vérifie les erreurs HTTP
 
-    # Décoder le contenu CSV
-    csv_data = response.text.splitlines()
+    # Décoder le contenu CSV avec l'encodage UTF-8
+    csv_data = response.content.decode('utf-8').splitlines()
     reader = csv.DictReader(csv_data)  # Utilise les noms de colonnes comme clés
 
     # Parser et afficher les données
@@ -40,20 +42,19 @@ def rando():
     for row in reader:
         try:
             parsed_row = {
-                "date": parse_date(row["Date"]).strftime('%Y-%m-%d'),
-                "start": row["Lieu de depart"],
-                "map": row["Parcours"],
-                "km": row["Km"],
-                "time": parse_time(row["Heure"]).strftime('%H:%M'),
+                "date": parse_date(row["date"]).strftime('%Y-%m-%d'),
+                "start": row.get("start", "Non définie"),
+                "map": row.get("parcours", "Non définie"),
+                "km": row.get("km", "Non définie"),
+                "time": row.get("time", "Non définie")
             }
             parsed_data.append(parsed_row)
         except (ValueError, KeyError) as e:
             print(f"Erreur lors du parsing de la ligne : {row} -> {e}")
     parsed_data = sorted(parsed_data, key=lambda x: (x["date"], x["time"]))
 
-
-    with open('../_data/cyclo_calendar.json', 'w') as json_file:
-        json.dump(parsed_data, json_file, indent=4)
+    with open('../_data/cyclo_calendar.json', 'w', encoding='utf-8') as json_file:
+        json.dump(parsed_data, json_file, indent=4, ensure_ascii=False)
 
     print("Données parsées sauvegardées dans 'cyclo_calendar.json'")
 
